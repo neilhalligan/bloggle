f9446c2fcac75dd8109c9ad1779bb2e1916a2f4a
Neil Halligan <neilhalligan2972@gmail.com>
2017-03-31 19:14:28 +0100

    added help, about, home pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+
+  get 'static_pages/about'
+  get 'static_pages/help'# => "static_pages#help"
+  # get 'dashboard' => 'dashboards#dashboard', as: 'dashboard'
+  # resources :static_pages, only: [:about, :help]
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
