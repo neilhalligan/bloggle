6ef05bda9c5d2ce6c268e6ff92c54a2d0b90c019
Neil Halligan <neilhalligan2972@gmail.com>
2017-04-01 15:13:18 +0100

    built title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
-
+  def full_title(title = "")
+    base_title = "Bloggle"
+    if title.empty?
+      base_title
+    else
+      "#{title} | #{base_title}"
+    end
+  end
 end
