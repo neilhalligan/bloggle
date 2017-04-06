6c3b03c8b4e49be36ab10c5ce25eb4fdeecb123b
Neil Halligan <neilhalligan2972@gmail.com>
2017-04-06 15:11:08 +0100

    about to delete old stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email.downcase! }
   validates :name, presence: :true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,9 +11,20 @@ class User < ApplicationRecord
   NO_SPACES_REGEX = Regexp.new '\A([^(\s)]{6,})\z'
   validates :password, length: { minimum: 6 }, format: { with: NO_SPACES_REGEX }
 
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,14 +5,4 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     get login_path
     assert_response :success
   end
-
-  test "should get create" do
-
-  end
-
-  test "should destroy session" do
-
-  end
-
-
 end
