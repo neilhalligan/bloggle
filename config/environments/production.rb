e0f5cf26dcb1d98f656a384bb3234dcc77848f2a
Neil Halligan <neilhalligan2972@gmail.com>
2017-04-27 13:52:46 +0100

    small changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain  e       => 'heroku.com',
+    :domain  e       => 'herokuapp.com',
     :enable_starttls_auto => true
   }
 end
