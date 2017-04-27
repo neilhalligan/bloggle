d9a68cc5af683ed5ec42a02e51096176ad000653
Neil Halligan <neilhalligan2972@gmail.com>
2017-04-27 13:57:25 +0100

    stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain:        'secret-stream-58448.herokuapp.com',
     :enable_starttls_auto => true
   }
 end
